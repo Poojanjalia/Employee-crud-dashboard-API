09719054d7fca7e9a246716daabe255f81096f65
unknown <jaliapoojan@gmail.com>
2025-11-26 23:07:30 +0530

    Initial commit for API project

---
+++ EMS_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using EMS_API;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Employee> Employees { get; set; }
+}
---
+++ EMS_API/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace EMS_API
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string EmployeeID { get; set; }
+        public string JobTitle { get; set; }
+        public string Department { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
+        public string EmploymentType { get; set; }
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContactPhone { get; set; }
+        public string Relationship { get; set; }
+    }
+}
